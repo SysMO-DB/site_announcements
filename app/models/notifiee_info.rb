305cca221bd2759a97c935871363acae0c54f0a9
Stuart Owen <stuart.owen@manchester.ac.uk>
2015-02-16 17:04:28 +0000

    updated to use uuid instead of uuidtools, and added the dependency to the gemspec

--- app/models/notifiee_info.rb
+++ app/models/notifiee_info.rb
@@ -1,4 +1,4 @@
-require 'uuidtools'
+require 'uuid'
 
 class NotifieeInfo < ActiveRecord::Base
   belongs_to :notifiee,:polymorphic=>true
@@ -11,7 +11,7 @@ class NotifieeInfo < ActiveRecord::Base
   
   def check_unique_key
     if self.unique_key.nil? || self.unique_key.blank?
-      self.unique_key = UUIDTools::UUID.random_create.to_s
+      self.unique_key = UUID.generate
     end
   end    
   
